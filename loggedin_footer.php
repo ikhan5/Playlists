32fc76e8ca2366cbb7740a2f681ba4ffd37fcf44
Imzan Khan <ikhan5@lakeheadu.ca>
2019-05-22 17:59:14 -0400

    Removed To do from footer

--- loggedin_footer.php
+++ loggedin_footer.php
@@ -2,19 +2,14 @@
     <div id="footer-container">
         <section id="footer-navigation">
             <nav class="navbar navbar-expand-sm navbar-dark" id="footer-nav">
-                <img alt="Logo for Get Together" src="/Content/Images/bluegglogo.png" class="header-logo"
-                    id="footer-logo">
+                <img alt="Logo for Get Together" src="/Content/Images/bluegglogo.png" class="header-logo" id="footer-logo">
                 <div class="collapse navbar-collapse" id="navbarColor01">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item active">
-                            <a class="nav-link navbar-custom" href="#top">Back to top <span
-                                    class="sr-only">(current)</span></a>
+                            <a class="nav-link navbar-custom" href="#top">Back to top <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link navbar-custom" href="#">Copyright</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link navbar-custom" href="db/listtodo.php">To Do</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link navbar-custom" href="#">Privacy & Confidentiality</a>
@@ -26,12 +21,9 @@
         <p>&copy; <?php echo date("Y"); ?> Echo Friends Design, All Rights Reserved</p>
     </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.4.0.min.js"
-    integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
-    integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
 </script>
 <script src="/scripts/songs.js"></script>
 <script src="/scripts/payments.js"></script>
